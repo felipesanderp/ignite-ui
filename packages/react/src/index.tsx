3e7e66e2ee28b3b1470ff4f6d1d164b85becb3e7
felipesanderp <felipe.panisson@hotmail.com>
2024-01-19 14:08:43 -0300

    button variants and dark theme

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,8 +1,30 @@
+import { VariantProps } from '@stitches/react'
 import { styled } from './styles'
 
 export const Button = styled('button', {
   fontFamily: '$default',
-  backgroundColor: '$ignite500',
+  backgroundColor: '$ignite300',
   borderRadius: '$md',
-  padding: '$4',
+  border: 0,
+  fontWeight: '$bold',
+  color: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
 })
+
+export type ButtonProps = VariantProps<typeof Button>
